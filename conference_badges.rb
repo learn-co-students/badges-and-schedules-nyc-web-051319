2269b81f1168c303838cbdcceb3ecdb38409611c
Elma Rahman <elmarahman635@gmail.com>
2019-04-16 01:41:20 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,40 +1,22 @@
-def badge_maker(speaker)
-
-  attendees.each do | speaker|
- puts "Hello, my name is #{speaker}."
-
+def badge_maker(name)
+ return "Hello, my name is #{name}."
 end
 
-def batch_badge_creator()
-#takes an array of names as an argument and returns an array of badge messages.
-#what badge msgs? if the badge_maker is doing that alrdy.
+def batch_badge_creator(attendees)
+  attendees.collect do | name |
+    badge_maker(name)
+end
 end
 
-def assign_rooms(room_assignments)
-  room_assignments.each do | room_assignments |
-
-  #puts "Hello, #{speaker}! You'll be assigned to room #{room_assignments}!"
-
-  (1..7).each.with_index(1) do |speaker, index|
-      puts "Hello, #{speaker}! You'll be assigned to room #{index}"
-  end
-
-  #if this is an array -- how can you print out each assgnmnt? -- need to
-  #iterate over your array of room assgnmnt in order to puts out individual assgnmnt.
-
-  #assign rm# to each person
-
-
+def assign_rooms(attendees)
+     attendees.collect.each_with_index do |name, index|#set for iteration, way to access each element in array
+   "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+end
 end
 
+#object your iterating over
 
-def printer(list_of_badges, room_assignments)
-
-  puts ""
-
-  #should puts list of badges and room_assignments
-
-#outputs result of batch_badge_creato
-#outputs result of assign_rooms
-end
+def printer (attendees)
+  batch_badge_creator(attendees).each {|string| puts string}
+   assign_rooms(attendees).each {|string| puts string}
 end
