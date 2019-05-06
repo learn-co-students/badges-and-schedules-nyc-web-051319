042fb39b1eb6129b2523a44d331977825390a992
Avitosh Totaram <a.totaram@gmail.com>
2019-05-06 00:37:55 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
 # Write your code here.
+
+def badge_maker(name)
+  return "Hello, my name is #{name}." 
+end
+
+def batch_badge_creator(attendees)
+  arr1 = []
+  attendees.each do |name|
+    arr1.push("Hello, my name is #{name}")
+  end
+  return arr1
+end
+
+def assign_rooms(attendees)
+  room = 1 
+  arr2 = []
+  while room < 8
+  attendees.each do |name|
+  arr2.push("Hello, #{name}! You'll be assigned to room _#{room}!"
+  room +=1
+end
+return arr2
+end
