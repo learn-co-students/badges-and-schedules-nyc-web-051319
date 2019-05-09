8306f04cb5e33fa36a9cb1a952ad362ae9e7c53c
yana.loginova <yana.loginova@cix.csi.cuny.edu>
2019-05-09 06:53:25 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,30 @@
 # Write your code here.
+def badge_maker(name) 
+  return "Hello, my name is #{name}."
+end
+def batch_badge_creator (array)
+ array.collect do |name|
+   badge_maker(name)
+end
+end
+
+def assign_rooms(guest)
+  new_array=[]
+  room =1
+  guest.each do |name|
+    # room+=1
+    
+    new_array.push("Hello, #{name}! You'll be assigned to room #{room}!")
+    room+=1
+  end
+    return new_array
+  end 
+  def printer (guest)
+    batch_badge_creator(guest).each do |i|
+    puts i
+  end
+  assign_rooms(guest).each do |i|
+    puts i  
+end
+end
+  
